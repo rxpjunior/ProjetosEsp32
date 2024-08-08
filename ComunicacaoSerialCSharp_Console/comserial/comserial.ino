f5ec0346b66b38920692e36e98fa216bb1caf7cf
rxpjunior <rxpjunior@gmail.com>
2024-08-08 06:42:57 -0300

    Subindo projeto consolo com Csharp

---
+++ ComunicacaoSerialCSharp_Console/comserial/comserial.ino
@@ -0,0 +1,54 @@
+void setup() {
+
+  Serial.begin(115200); // Inicializa a porta serial
+
+  pinMode(2, OUTPUT); // Configura o pino 2 como saída (LED acoplado no ESP32)
+
+}
+
+void loop() {
+
+  char mensagem[10]; // Cria um array de caracteres para armazenar a mensagem recebida
+
+  int i = 0; // Inicializa um índice para o array
+
+  // Verifica se há dados disponíveis na porta serial
+
+  if (Serial.available()) {
+
+    // Lê o próximo byte recebido na porta serial
+
+    char c = Serial.read();
+
+    // Armazena o byte lido no array de mensagem
+
+    mensagem[i] = c;
+
+    // Incrementa o índice do array
+
+    i++;
+
+    // Acende o LED conectado ao pino 2
+
+    digitalWrite(2, HIGH);
+
+    delay(500); // Espera 500 milissegundos
+
+    // Apaga o LED conectado ao pino 2
+
+    digitalWrite(2, LOW);
+
+    delay(500); // Espera 500 milissegundos
+
+    Serial.flush();
+
+  }
+
+  // Adiciona o caractere nulo ao final do array para marcar o fim da string
+
+  mensagem[i] = '\0';
+
+  // Envia a mensagem de volta pela porta serial
+
+  Serial.write(mensagem);
+}
